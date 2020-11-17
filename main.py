0d4d015ef59ff93fd207d271f89acd7efa518633
AdaSzmygin <74568364+AdaSzmygin@users.noreply.github.com>
2020-11-17 22:50:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print('fff')
+f = open('C:\studia\semestr V\zinformatyzowane systemy katastralne\Kontury_eksport_dz.txt')
 
-print('oooo')
+bledy = []
+
+tab = []
+for linie in f:
+    e = linie.split()
+    if 0 < len(e) < 7 and (len(e) !=1 or len(e[0])>2):  #poprawic troche warunek, zeby nie bylo 138
+      #  print(e[0:2])
+        tab.append(e)
+
+for i in range(len(tab)):
+    #print(tab[i][0])
+    if ('-') in tab[i][0]:
+        bledy.append(3)
+    #    print(' ')
+        #print((tab[i][0]).index('-'))
+ #       print('ok')
+    else:
+   #     print('blad')
+         print(tab[i])
+     #   bledy.append(tab[i])
